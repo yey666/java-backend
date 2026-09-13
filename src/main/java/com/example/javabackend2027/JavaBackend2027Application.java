d9a78614af910156aa019ac930f43ec68f712fad
yey666 <3969886822@qq.com>
2026-09-13 17:05:47 +0800

    Hello World 接口跑通

---
+++ src/main/java/com/example/javabackend2027/JavaBackend2027Application.java
@@ -0,0 +1,13 @@
+package com.example.javabackend2027;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackend2027Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackend2027Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javabackend2027/JavaBackend2027ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javabackend2027;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackend2027ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
